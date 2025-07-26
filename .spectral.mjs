f0b82ff6d6fed27cb84984c21ba34751be224cff
Julien <julienpmjacquet@gmail.com>
2025-07-26 13:09:01 -0400

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI4ODA0MA/spectral.js?branch_slug=main&token=5d63fd16-cee4-48e6-80cd-aa1927b4a4e5";
+export default { extends: ruleset };
